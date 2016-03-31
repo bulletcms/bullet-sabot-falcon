0de90d1c59f3de0aa409bf329ddee1ae0bd73e97
Kevin Wang <wangkevin448@gmail.com>
2016-03-30 18:35:41 -0700

    rkt not deploying

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-./venv/bin/python src/app.py
+# ./venv/bin/python src/app.py
+
+version="v1.2.1"
+
+if ! [ -e "$PWD/rkt/rkt" ]; then
+  wget "https://github.com/coreos/rkt/releases/download/$version/rkt-$version.tar.gz" -O rkt.tar.gz
+  tar -xvzf rkt.tar.gz -C "$PWD/rkt" --strip-components=1
+  rm rkt.tar.gz
+fi
+
+if ! [ -e "$PWD/bullet-sabot.aci" ]; then
+  ./build.sh
+fi
+
+./rkt/rkt --insecure-options=image run ./bullet-sabot.aci --volume static,kind=host,source=/home/kevin/projects/webdev/bullet-tracer/dist,readOnly=true --net=host
