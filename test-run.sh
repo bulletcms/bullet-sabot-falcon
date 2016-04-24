51dc882bedc7ec569c742431c8c65360ef8a233e
Kevin Wang <wangkevin448@gmail.com>
2016-04-24 00:14:44 -0700

    test run

--- test-run.sh
+++ test-run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+DATASTORE_DATASET=bullet-sabot-test
+DATASTORE_HOST=http://localhost:6000
+DATASTORE_EMULATOR_HOST=localhost:6000
+DATASTORE_PROJECT_ID=bullet-sabot-test
 
 ./venv/bin/python src/app.py
