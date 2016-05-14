6c4e60200a1def095b703179cc6202a442b45ad3
Kevin Wang <wangkevin448@gmail.com>
2016-05-14 11:54:54 -0700

    using hooks

---
+++ bulletsabot/bulletapi/falext/status.py
@@ -0,0 +1,17 @@
+from importlib import import_module
+
+STATUSES = set(200, 201, 204, 400)
+
+HTTPSTATUS = {}
+
+def httpstatus(code):
+    '''
+    :param code: 3 digit html status code
+    '''
+    assert isinstance(code, int) and code > 99 and code < 1000
+
+    if code in HTTPSTATUS:
+        return HTTPSTATUS[code]
+    elif code in STATUSES:
+        HTTPSTATUS[code] = import_module('falcon.HTTP_{0}'.format(code))
+        return HTTPSTATUS[code]
--- bulletsabot/bulletapi/pages.py
+++ bulletsabot/bulletapi/pages.py
@@ -1,5 +1,6 @@
 import ujson
-from falcon import HTTPNotFound, HTTPError, HTTP_200, HTTP_201, HTTP_204, HTTP_400
+from falcon import HTTPNotFound, HTTPBadRequest, before
+from falext import httpstatus
 from .module import Fmod
 
 
@@ -7,12 +8,9 @@ from .module import Fmod
 ##    Pages API                         ##
 ##########################################
 
-def abort_if_page_dne(func):
-    def decorated_func(self, req, res, page_id):
-        if not self._data_service.has_page(page_id):
-            raise HTTPNotFound()
-        func(self, req, res, page_id)
-    return decorated_func
+def abort_if_page_dne(req, resp, resource, params):
+    if not resource._data_service.has_page(params['page_id']):
+        raise HTTPNotFound()
 
 
 # PAGE COLLECTION RESOURCE
@@ -21,7 +19,7 @@ class PagesCollection:
         self._data_service = data_service
 
     def on_get(self, req, res):
-        res.status = HTTP_200
+        res.status = httpstatus(200)
         res.body = ujson.dumps({'data': self._data_service.get_pagelist()})
 
 
@@ -30,37 +28,37 @@ class Page:
     def __init__(self, data_service):
         self._data_service = data_service
 
-    @abort_if_page_dne
+    @before(abort_if_page_dne)
     def on_get(self, req, res, page_id):
-        res.status = HTTP_200
+        res.status = httpstatus(200)
         res.body = ujson.dumps({'data': self._data_service.get_page(page_id)})
 
-    @abort_if_page_dne
+    @before(abort_if_page_dne)
     def on_delete(self, req, res, page_id):
         self._data_service.remove_page(page_id)
-        res.status = HTTP_204
+        res.status = httpstatus(204)
         res.body = ujson.dumps({'data': 'deleted page {}'.format(page_id)})
 
     def on_post(self, req, res, page_id):
         try:
             raw_json = req.stream.read()
         except Exception as ex:
-            raise HTTPError(HTTP_400, 'Error', ex.message)
+            raise HTTPBadRequest('Error', ex.message)
 
         try:
             data = ujson.loads(raw_json, encoding='utf-8')['data']
             self._data_service.add_page(page_id, data['title'], data['tags'], data['content'])
-            res.status = HTTP_201
+            res.status = httpstatus(201)
             res.body = ujson.dumps({'data': 'posted page {}'.format(page_id)})
         except ValueError:
-            raise HTTPError(HTTP_400, 'Invalid JSON', 'Could not decode the request body.')
+            raise HTTPBadRequest('Invalid JSON', 'Could not decode the request body')
 
-    @abort_if_page_dne
+    @before(abort_if_page_dne)
     def put(self, req, res, page_id):
         try:
             raw_json = req.stream.read()
         except Exception as ex:
-            raise HTTPError(HTTP_400, 'Error', ex.message)
+            raise HTTPBadRequest('Error', ex.message)
 
         try:
             data = ujson.loads(raw_json, encoding='utf-8')['data']
@@ -74,10 +72,10 @@ class Page:
             if 'content' in data:
                 content = data['content']
             self._data_service.update_page(page_id, title, tags, content)
-            res.status = HTTP_201
+            res.status = httpstatus(201)
             res.body = ujson.dumps({'data': 'put page {}'.format(page_id)})
         except ValueError:
-            raise HTTPError(HTTP_400, 'Invalid JSON', 'Could not decode the request body.')
+            raise HTTPBadRequest('Invalid JSON', 'Could not decode the request body.')
 
 
 MAGIC_DATA_SERVICE = 'data_service'
