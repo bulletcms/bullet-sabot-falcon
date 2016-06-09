d083fd617a0068910d9df34c1403cf7193bf8c3a
Kevin Wang <wangkevin448@gmail.com>
2016-06-08 23:46:36 -0700

    not using dist directory anymore

--- start3.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d -p 5001:5000 -v /home/kevin/projects/web/bullet-tracer/dist:/app/static --name bullet-sabot-container-1 bullet-sabot
-docker run -d -p 5002:5000 -v /home/kevin/projects/web/bullet-tracer/dist:/app/static --name bullet-sabot-container-2 bullet-sabot
-docker run -d -p 5003:5000 -v /home/kevin/projects/web/bullet-tracer/dist:/app/static --name bullet-sabot-container-3 bullet-sabot
